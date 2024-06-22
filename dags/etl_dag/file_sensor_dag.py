ca2ce50567655ce8ebc31cb3abfc38cf392d83b5
Dinesh Singh <dineshpsingh16@gmail.com>
2024-06-22 12:33:31 +0530

    fixing dag's issue

--- dags/etl_dag/file_sensor_dag.py
+++ dags/etl_dag/file_sensor_dag.py
@@ -40,16 +40,24 @@ file_sensor = FileSensor(
 )
 
 # Define the BashOperator task to create a unique virtual environment, install Pandas, and process the file
+# process_file = BashOperator(
+#     task_id='process_file',
+#     bash_command=f"""
+#     VENV_DIR=/tmp/venv_{{ ti.dag_id }}_{{ ti.task_id }} && \
+#     python3 -m venv $VENV_DIR && \
+#     source $VENV_DIR/bin/activate && \
+#     pip install pandas reportlab && \
+#     python {dags_folder}/repo/dags/etl_dag/process_file.py && \
+#     deactivate && \
+#     rm -rf $VENV_DIR
+#     """,
+#     dag=dag,
+# )
+
 process_file = BashOperator(
     task_id='process_file',
     bash_command=f"""
-    VENV_DIR=/tmp/venv_{{ ti.dag_id }}_{{ ti.task_id }} && \
-    python3 -m venv $VENV_DIR && \
-    source $VENV_DIR/bin/activate && \
-    pip install pandas reportlab && \
-    python {dags_folder}/repo/dags/etl_dag/process_file.py && \
-    deactivate && \
-    rm -rf $VENV_DIR
+    pwd && ls -ltr 
     """,
     dag=dag,
 )
