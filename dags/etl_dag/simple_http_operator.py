6acd61cda3825331dcc12702f4bcca9d415ad90f
Dinesh Singh <dineshpsingh16@gmail.com>
2024-06-24 17:53:23 +0530

    debug dag

--- dags/etl_dag/simple_http_operator.py
+++ dags/etl_dag/simple_http_operator.py
@@ -2,7 +2,7 @@
 ### Example HTTP operator and sensor
 """
 from airflow import DAG
-from airflow.operators import SimpleHttpOperator, HttpSensor
+from airflow.operators import  HttpSensor
 from datetime import datetime, timedelta
 import json
 
@@ -25,44 +25,6 @@ dag = DAG('example_http_operator', default_args=default_args)
 dag.doc_md = __doc__
 
 # t1, t2 and t3 are examples of tasks created by instatiating operators
-t1 = SimpleHttpOperator(
-    task_id='post_op',
-    endpoint='api/v1.0/nodes',
-    data=json.dumps({"priority": 5}),
-    headers={"Content-Type": "application/json"},
-    response_check=lambda response: True if len(response.json()) == 0 else False,
-    dag=dag)
-
-t5 = SimpleHttpOperator(
-    task_id='post_op_formenc',
-    endpoint='nodes/url',
-    data="name=Joe",
-    headers={"Content-Type": "application/x-www-form-urlencoded"},
-    dag=dag)
-
-t2 = SimpleHttpOperator(
-    task_id='get_op',
-    method='GET',
-    endpoint='api/v1.0/nodes',
-    data={"param1": "value1", "param2": "value2"},
-    headers={},
-    dag=dag)
-
-t3 = SimpleHttpOperator(
-    task_id='put_op',
-    method='PUT',
-    endpoint='api/v1.0/nodes',
-    data=json.dumps({"priority": 5}),
-    headers={"Content-Type": "application/json"},
-    dag=dag)
-
-t4 = SimpleHttpOperator(
-    task_id='del_op',
-    method='DELETE',
-    endpoint='api/v1.0/nodes',
-    data="some=data",
-    headers={"Content-Type": "application/x-www-form-urlencoded"},
-    dag=dag)
 
 sensor = HttpSensor(
     task_id='http_sensor_check',
@@ -73,8 +35,4 @@ sensor = HttpSensor(
     poke_interval=5,
     dag=dag)
 
-t1.set_upstream(sensor)
-t2.set_upstream(t1)
-t3.set_upstream(t2)
-t4.set_upstream(t3)
-t5.set_upstream(t4)
+sensor
